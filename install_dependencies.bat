4d7ca92a5fdf8f42ed03b3556e66ac93173ef2a1
Michael Ellicott <mellic03@gmail.com>
2021-12-21 12:32:42 +1000

    added python to installer and check 32 vs 64 bit

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,18 +1,33 @@
 @echo off
 
-pip install -r dependencies\requirements.txt
+echo A few dependencies have to be met in order for the bot to work, this setup file exists to make getting those dependencies easier
+pause
 
-echo Ignore aprturl error if you have one
-echo.
+echo First, python has to be installed, make sure to check all optional boxes during the installation
+pause
+echo Running python installer
+echo Come back to this window after python has finished installing
 
-echo.
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT dependencies\python-3.10.1.exe
+if %OS%==64BIT dependencies\python-3.10.1-amd64.exe
+
 
 echo Next you need to install visual c++ build tools, that will open now.
 pause
 
 dependencies\vs_buildtools.exe
+echo.
+
+echo Continue to install python libraries
+pause
+pip install -r dependencies\requirements.txt
+echo Ignore aprturl error if you have one
 
 echo.
+echo.
+
 echo Finished!
 pause
 exit
